b8e3271a23627e343bc126ad5cea3c22863c30aa
Tarun Rana <66361151+tarun264@users.noreply.github.com>
2023-10-02 02:19:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function volume_sphere() {
     //Write your code here
+	let radius= document.getElementById('radius').value;
+	radius=Math.abs(radius);
+
+	let volume=4/3*Math.PI*Math.pow(radius,3);
+
+	let calculate=document.getElementById('volume');
+	calculate.value=volume;
+	return false;
+	
+
+
+	
+	
+
+	
   
 } 
 
